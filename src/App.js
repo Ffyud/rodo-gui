dd4d0a6189bda84387e1a02f763c4d9766deae8e
Ffyud <5263778+Ffyud@users.noreply.github.com>
2023-01-20 11:36:14 +0100

    text

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header><h1>RoDo spielerij</h1><h5>Wie is de sjaak?</h5></header>
+      <header><h1>🔥 RoDo 🔥</h1><h5>Wie is de sjaak?</h5></header>
       <main>
         Ja man
+        <ul>
+          <li>Inloggen met je team en op 1 apparaat opdrachten invullen</li>
+          <li>Individueel inloggen en je briefje invullen</li>
+        </ul>
       </main>
     </div>
   );
