c85dc773acfac51a2085fed9d45509514fa42efc
WhenIf <donald_dezeeuw@hotmail.com>
2023-01-26 20:17:07 +0100

    wat tekst weg

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
       <header><h1>🔥 RoDo 🔥</h1><h5>Wie is de sjaak?</h5></header>
       <main>
         Ja man
-        <ul>
-          <li>Inloggen met je team en op 1 apparaat opdrachten invullen</li>
-          <li>Individueel inloggen en je briefje invullen</li>
-        </ul>
+        Hoi
       </main>
     </div>
   );
