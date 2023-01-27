5d56904173601a9a0c817a0574cc8860cdb55c88
David <5263778+Ffyud@users.noreply.github.com>
2023-01-27 09:39:01 +0100

    voegt HashRouter toe zodat routes werken

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // FIXME HashRouter uiteindelijk vervangen, maar nu nog even nodig. Geeft "/#/" in URL in browser
+    <React.StrictMode>
+        <HashRouter> 
+            <App />
+        </HashRouter>
+    </React.StrictMode>
 );
