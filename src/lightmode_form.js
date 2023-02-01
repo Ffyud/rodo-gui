42efc2e66cc5aad9e0911ea670c311c067107b0d
WhenIf <donald_dezeeuw@hotmail.com>
2023-02-01 22:28:14 +0100

    spelen met darkmode

---
+++ src/lightmode_form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./mode_form.css";
+const LightModeForm = (props) => {
+  return (
+    <form onSubmit={props.handleAddQuestion}>
+      <h1>Light Mode Form</h1>
+      {props.questions.map((question) => (
+        <div key={question.id}>
+          <label>{question.text}</label>
+          <input type="text" name={question.id} onChange={props.handleChange} />
+        </div>
+      ))}
+      <input type="text" name="question" />
+      <button>Add Question</button>
+    </form>
+  );
+};
+export default LightModeForm;
