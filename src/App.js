db514a43d66dcf5a0de4b8bb81fba68c8560efc2
David <5263778+Ffyud@users.noreply.github.com>
2023-06-21 19:50:48 +0200

    fix merging issue

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import Vragenlijst from "./Vragenlijst";
-import Quiz from "./Deelnemer_VraagFrameWork";
 import React from "react";
 import Questionnaire from "./Vragenlijst_aanmaken";
 import LoginForm from "./LogIn";
-import MultiQuestion from "./testMultiChoice";
 
 function App() {
     return (
@@ -17,9 +15,6 @@ function App() {
                     <Route path="/vragenlijst" element={<Vragenlijst vragenlijstId="1" />} />
                     <Route path="/vragenlijst_aanmaken" element={<Questionnaire />} />
                     <Route path="/login" element={<LoginForm />} />
-                    <Route path="vragenlijst" element={<Vragenlijst />} />
-                    <Route path="Deelnemer_VraagFrameWork" element={<Quiz />} />
-                    <Route path="testMultiChoice" element={<div className="question-container"><MultiQuestion /></div>} />
                 </Route>
             </Routes>
 
