30012674c200eb08ef736fdfcbaab8ea6d1fdfa4
David <5263778+Ffyud@users.noreply.github.com>
2023-06-21 19:44:34 +0200

    add dummy questionList

--- src/services/questionlist.js
+++ src/services/questionlist.js
@@ -3,26 +3,27 @@
 
 // TODO deze "services" constructie proberen (https://medium.com/nerd-for-tech/react-js-services-854be54a6ba1)
 
-const getQuestionList = (questionlistId) => {
-    const team = [
-        {
-            "id": 1,
-            "name": "eerste lijst",
-            "createdAt": "2023-02-26T15:52:59.673Z",
-            "updatedAt": "2023-02-26T15:52:59.673Z",
-            "Questions": [
-                {
-                    "id": 1,
-                    "content": "Regent het ook?",
-                    "createdAt": "2023-02-26T15:53:03.822Z",
-                    "updatedAt": "2023-02-26T15:53:03.822Z",
-                    "QuestionListId": 1
-                }
-            ]
-        }
-    ];
+const getQuestionList = {
+        id: 1,
+        name: "eerste lijst",
+        createdAt: "2023-02-26T15:52:59.673Z",
+        updatedAt: "2023-02-26T15:52:59.673Z",
+        Questions: [
+            {
+                id: 1,
+                content: "Regent het ook?",
+                createdAt: "2023-02-26T15:53:03.822Z",
+                updatedAt: "2023-02-26T15:53:03.822Z",
+                QuestionListId: 1,
+            },
+            {
+                id: 2,
+                content: "Drink je bier?",
+                createdAt: "2023-02-26T15:53:03.822Z",
+                updatedAt: "2023-02-26T15:53:03.822Z",
+                QuestionListId: 1,
+            },
+        ],
+    };
 
-   return team;
-}
-
-export default getQuestionList;
+// export default getQuestionList;
